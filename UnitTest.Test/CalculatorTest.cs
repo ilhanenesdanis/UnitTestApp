e8759d47bee210fa4faf513e876be96ff5e42366
ilhan enes daniş <danisenes3@gmail.com>
2022-08-07 14:30:18 +0300

    Add project files.

---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestApp;
+using Xunit;
+
+namespace UnitTest.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 10;
+            var calculator = new Calculator();
+            //Act
+            var total = calculator.add(a, b);
+            //Assert
+            Assert.Equal<int>(15, total);
+
+        }
+    }
+}
